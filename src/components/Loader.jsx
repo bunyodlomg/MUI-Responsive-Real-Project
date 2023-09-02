40134c4915c9a2443f6ec9de0d0d9cb92711a65a
bunyodlomg <bunyodlomg@gmail.com>
2023-09-02 09:21:00 +0500

    1

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,4 +1,3 @@
-import { Translate } from '@mui/icons-material'
 import { Box, CircularProgress } from '@mui/material'
 import React from 'react'
 
@@ -6,7 +5,6 @@ const Loader = () => {
     return (
         <Box
             sx={{
-                // my: 10,
                 position: 'fixed',
                 bottom: 100,
                 translate: '5px 0px',
